eddfac045b8c5a3ece35799a62a5f68ab933dd32
oashmoore <olivia@energyinnovation.org>
2023-05-10 15:15:04 -0700

    ira modeling

--- GeneratedDataLoggingScript.cmd
+++ GeneratedDataLoggingScript.cmd
@@ -1,10 +1,16 @@
 SPECIAL>LOADMODEL|"EPS.mdl"
 
-SIMULATE>SAVELIST|OutputVarsToExport-Calibration2.lst
+SIMULATE>SAVELIST|OutputVarsToExport.lst
 SIMULATE>RUNNAME|NoSettings
 SIMULATE>READCIN|
 MENU>RUN|O
-MENU>VDF2TAB|NoSettings.vdfx|NoSettings.tsv|OutputVarsToExport-Calibration2.lst|+!||2020|2050|:NoSettings
+MENU>VDF2TAB|NoSettings.vdfx|NoSettings.tsv|OutputVarsToExport.lst|+!||2020|2050|:NoSettings
 FILE>DELETE|NoSettings.vdfx
+
+SIMULATE>RUNNAME|IRA_Moderate_Incentives
+SIMULATE>READCIN|IRA_Moderate_Incentives.cin
+MENU>RUN|O
+MENU>VDF2TAB|IRA_Moderate_Incentives.vdfx|IRA_Moderate_Incentives.tsv|OutputVarsToExport.lst|+!||2020|2050|:IRA_Moderate_Incentives
+FILE>DELETE|IRA_Moderate_Incentives.vdfx
 
 SIMULATE>SAVELIST|
